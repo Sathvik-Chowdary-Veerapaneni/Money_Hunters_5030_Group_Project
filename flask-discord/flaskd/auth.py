d707fe3519331c7e927277fd134b0999fdae7af2
Sathvik-Chowdary-Veerapaneni <98241593+Sathvik-Chowdary-Veerapaneni@users.noreply.github.com>
2022-04-22 22:05:51 -0500

    Bug fixed on accessing settings page
    Bug:
          -User accessing the '/auth/settings' page directly without logging in.   
    Solved:
          - By adding the decorator @login_required  to the settings route.
          - Now, the User is restricted to access the '/auth/settings' page without logging in.
          - If a user tried to access '/auth/settings' without logging in it will redirect to the login page.

--- flask-discord/flaskd/auth.py
+++ flask-discord/flaskd/auth.py
@@ -9,8 +9,17 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flaskd.db import get_db
 
 bp = Blueprint('auth',__name__,url_prefix='/auth')
-#
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))  
+        return view(**kwargs)
+    return wrapped_view
+
 @bp.route("/settings",methods=("GET","POST"))
+@login_required
 def change_username():
     if request.method == "POST":
         new_username=request.form["uname"]
@@ -90,12 +99,3 @@ def load_logged_in_user():
                 'SELECT * FROM user WHERE id = ?', (user_id,)
                 ).fetchone() # Grabs all columns in the user table where user_id is found. Load this in the session
 
-def login_required(view):
-    @functools.wraps(view)
-    def wrapped_view(**kwargs):
-        if g.user is None:
-            return redirect(url_for('auth.login'))
-        
-        return view(**kwargs)
-    
-    return wrapped_view
